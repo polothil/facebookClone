afd73ad71e2cbc75440a6e727ee68fd7100ef850
aravindpvk <aravindpvk@gmail.com>
2022-05-26 21:26:54 +0200

    login completed on backend

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -7,6 +7,8 @@ const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const { generateToken } = require('../helpers/tokens');
 const { sendVerificationEmail } = require('../helpers/mailer');
+const jwt = require('jsonwebtoken');
+
 exports.register = async (req, res) => {
   try {
     const { first_name, last_name, email, password, bYear, bMonth, bDay, gender } =
@@ -64,6 +66,63 @@ exports.register = async (req, res) => {
     const url = `${process.env.BASE_URL}/activate/${emailVerificationToken}`;
     sendVerificationEmail(user.email, user.first_name, url);
 
+    const token = generateToken({ id: user._id.toString() }, '7d');
+    res.send({
+      id: user._id,
+      username: user.username,
+      picture: user.picture,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      token: token,
+      verified: user.verified,
+      message: 'Registration Successful! Please verify your email to start',
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+exports.activateAccount = async (req, res) => {
+  try {
+    const { token } = req.body;
+    const user = jwt.verify(token, process.env.TOKEN_SECRET);
+    const check = await User.findById(user.id);
+    if (check.verified === true) {
+      return res.status(400).json({
+        message: 'This account is already verified',
+      });
+    } else {
+      await User.findByIdAndUpdate(user.id, {
+        verified: true,
+      });
+      return res.status(200).json({
+        message: 'Account verified successfully',
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        message: 'The email you entered is not connected to an account.',
+      });
+    }
+    const check = await bcrypt.compare(password, user.password);
+    if (!check) {
+      return res.status(400).json({
+        message: 'Invalid credentials. Please try again.',
+      });
+    }
     const token = generateToken({ id: user._id.toString() }, '7d');
     res.send({
       id: user._id,
