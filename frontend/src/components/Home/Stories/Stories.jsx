b05dd4387b54359fc73777f5fae7fd615e7cba1c
aravindpvk <aravindpvk@gmail.com>
2022-06-06 23:17:07 +0200

    Vid 59

--- frontend/src/components/Home/Stories/Stories.jsx
+++ frontend/src/components/Home/Stories/Stories.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { ArrowRight, Plus } from '../../../svg';
 import { stories } from '../../../data/home';
-import './Stories.css';
+import { useMediaQuery } from 'react-responsive';
 import Story from './Story/Story';
+import './Stories.css';
 
 const Stories = () => {
+  const query1175px = useMediaQuery({
+    query: '(max-width: 1175px)',
+  });
+  const query1030px = useMediaQuery({
+    query: '(max-width: 1030px)',
+  });
+  const max = query1030px ? 5 : query1175px ? 4 : stories.length;
   return (
     <div className='stories'>
       <div className='create_story_card'>
@@ -14,7 +22,7 @@ const Stories = () => {
         </div>
         <div className='story_create_text'>Create Story</div>
       </div>
-      {stories.map((story, index) => (
+      {stories.slice(0, max).map((story, index) => (
         <Story story={story} key={index} />
       ))}
       <div className='white_circle'>
