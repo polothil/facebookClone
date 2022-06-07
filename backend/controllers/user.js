42bea08d2ef12e1aad538de7b8819b9d933ab099
aravindpvk <aravindpvk@gmail.com>
2022-06-07 22:16:05 +0200

    Vid 64

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -86,9 +86,17 @@ exports.register = async (req, res) => {
 
 exports.activateAccount = async (req, res) => {
   try {
+    const validUserId = req.user.id;
     const { token } = req.body;
     const user = jwt.verify(token, process.env.TOKEN_SECRET);
     const check = await User.findById(user.id);
+
+    if (validUserId !== user.id) {
+      return res.status(400).json({
+        message: "You don't have the autorization to complete this operation.",
+      });
+    }
+
     if (check.verified === true) {
       return res.status(400).json({
         message: 'This account is already verified',
