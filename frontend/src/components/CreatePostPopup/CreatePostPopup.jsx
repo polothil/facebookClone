650e9cf89e8b9bebc721a93f8be5e224a2db2caf
aravindpvk <aravindpvk@gmail.com>
2022-06-09 12:57:35 +0200

    Vid 76

--- frontend/src/components/CreatePostPopup/CreatePostPopup.jsx
+++ frontend/src/components/CreatePostPopup/CreatePostPopup.jsx
@@ -1,9 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import Picker from 'emoji-picker-react';
 import './CreatePostPopup.css';
 
 const CreatePostPopup = ({ user }) => {
   const [text, setText] = useState('');
   const [showPrev, setShowPrev] = useState(false);
+  const [picker, setPicker] = useState(false);
+  const [cursorPosition, setCursorPosition] = useState(null);
+  const textref = useRef(null);
+
+  useEffect(() => {
+    textref.current.selectionEnd = cursorPosition;
+  }, [cursorPosition]);
+
+  const handleEmoji = (e, { emoji }) => {
+    const ref = textref.current;
+    ref.focus();
+    const start = text.substring(0, ref.selectionStart);
+    const end = text.substring(ref.selectionStart);
+    const newText = start + emoji + end;
+    setText(newText);
+    setCursorPosition(start.length + emoji.length);
+  };
+
   return (
     <div className='blur'>
       <div className='postBox'>
@@ -30,6 +49,7 @@ const CreatePostPopup = ({ user }) => {
         {!showPrev && (
           <div className='flex_center'>
             <textarea
+              ref={textref}
               maxLength='100'
               value={text}
               placeholder={`What's on your mind, ${user.first_name}`}
@@ -39,7 +59,16 @@ const CreatePostPopup = ({ user }) => {
           </div>
         )}
         <div className='post_emojis_wrap'>
-          <div className='comment_emoji_picker rlmove'></div>
+          {picker && (
+            <div className='comment_emoji_picker rlmove'>
+              <Picker onEmojiClick={handleEmoji} />
+            </div>
+          )}
+          <img src='../../../icons/colorful.png' alt='' />
+          <i
+            className='emoji_icon_large'
+            onClick={() => setPicker((prevState) => !prevState)}
+          ></i>
         </div>
       </div>
     </div>
